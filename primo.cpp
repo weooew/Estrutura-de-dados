a9a2e29a82f990915700ed7a41647676c075bfe0
weooew <102996679+weooew@users.noreply.github.com>
2022-09-22 06:49:46 -0500

    Update and rename primo.md to primo.cpp

--- primo.cpp
+++ primo.cpp
@@ -1,4 +1,28 @@
-```
+/*
+Faça uma função chamada primo que recebe um inteiro positivo n e retorna true (verdadeiro) se n é primo, ou false (falso) caso contrário.
+Entrada:
+* 1a linha: valor do inteiro positivo A
+* 2a linha: valor do inteiro positivo B (com A≤B).
+
+Saída:
+* Todos os primos de A até B (um por linha).
+---
+Exemplos
+>>
+3
+11
+<<
+3
+5
+7
+11
+---
+>>
+4
+10
+<<
+5
+7*/
 #include <iostream>
 using namespace std;
 
@@ -23,4 +47,3 @@ int main(){
     }
     
 }
-```
