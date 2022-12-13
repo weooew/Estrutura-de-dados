911ef5e31060d54525023986422aeb4f88684f73
weooew <102996679+weooew@users.noreply.github.com>
2022-12-13 20:20:06 -0300

    Update maximo_recursivo.cpp

--- maximo_recursivo.cpp
+++ maximo_recursivo.cpp
@@ -3,16 +3,18 @@
 using namespace std;
 
 int maximo(int n, int v[]) {
-	if (n == 1)
-		return v[0];
-	else {
-		int max;
-		max = maximo(n-1, v); // max eh o maximo de v[0..n-2]
-		if (max > v[n-1])
-			return max;
-		else
-			return v[n-1];
-	}
+   if (n == 1)
+      return v[0];
+   else {
+      int max;
+      max = maximo(n-1, v);
+	   
+      if (max > v[n-1])
+         return max;
+      else
+         return v[n-1];
+//return (maior > v[n-1]) ? maior : v[n-1];
+   }
 }
 
 int main () {
@@ -21,3 +23,16 @@ int main () {
 	cout << maximo(n, v) << endl;
 	return 0;
 }
+/*ou 
+int maximo1(int ini, int fim, int v[]) {
+    if(ini == fim) {
+        return v[ini];
+    }
+    else {
+        int x = (ini + fim) / 2;
+        int max1 = maximo1(ini, x, v);
+        int max2 = maximo1(x+1, fim, v);
+        return (max1 > max2) ? max1 : max2;
+    }
+}
+*/
